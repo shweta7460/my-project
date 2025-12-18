63838fe00c810d408da2a82ac430532e96040140
shweta7460 <cocomocotoon@gmail.com>
2025-12-18 13:29:03 -0500

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserCard } from './user-card/user-card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,UserCard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('My-Demo-Application');
+
+   shweta = "1234";
+
+
+
 }
---
+++ src/app/user-card/user-card.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-user-card',
+  imports: [],
+  templateUrl: './user-card.html',
+  styleUrl: './user-card.css',
+  encapsulation:ViewEncapsulation.None
+
+})
+export class UserCard {
+
+}
